e1623f3b4d4f99cc3050d1518e85edb05037bafc
serheihimself <sergm080@gmail.com>
2023-01-15 15:17:21 +0200

    render personal country commit

--- src/index.js
+++ src/index.js
@@ -46,3 +46,20 @@ input.addEventListener(
     }).join('');
   countryList.innerHTML = markup;
   }
+
+  function renderOneCountry(countries) {
+    const markup = countries
+      .map(country => {
+        return `<li>
+    <img src="${country.flags.svg}" alt="Flag of ${
+          country.name.official
+        }" width="30" hight="20">
+       <b>${country.name.official}</b></p>
+          <p><b>Capital</b>: ${country.capital}</p>
+          <p><b>Population</b>: ${country.population}</p>
+          <p><b>Languages</b>: ${Object.values(country.languages)} </p>
+              </li>`;
+      })
+      .join('');
+    countryList.innerHTML = markup;
+}
