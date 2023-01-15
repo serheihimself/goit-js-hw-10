779e8da020cc37cc6b25e65c7be9b0945810fec3
serheihimself <sergm080@gmail.com>
2023-01-15 15:02:40 +0200

    addEvListener commit

--- src/index.js
+++ src/index.js
@@ -7,3 +7,29 @@ const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener(
+    'input',
+    debounce(e => {
+        const trimmedValue = input.value.trim();
+           cleanHtml();   
+      if (trimmedValue !== '') {
+          fetchCountries(trimmedValue).then(foundData => {      
+  
+          if (foundData.length > 10) {
+            Notiflix.Notify.info(
+              'Too many matches found. Please enter a more specific name.'
+            );
+          } else if (foundData.length === 0) {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+          } else if (foundData.length >= 2 && foundData.length <= 10) {
+           
+            renderCountryList(foundData);
+          } else if (foundData.length === 1) {
+      
+            renderOneCountry(foundData);
+          }
+        });
+      }
+    }, DEBOUNCE_DELAY)
+  );
